ec72dac12f4f628f9f69704c3c0e24c27ff72d54
Harutin1991 <harut.soghomonyan@gmail.com>
2022-09-10 16:33:02 +0400

    Inint commit

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,351 @@
+@extends('layouts.site')
+
+@section('content')
+<section class="breadcrumb breadcrumb_bg">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="breadcrumb_iner">
+<div class="breadcrumb_iner_item">
+<p>Home. About</p>
+<h2>About us</h2>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="about_part single_about_page padding_bottom">
+<div class="container">
+<div class="row align-items-center justify-content-center">
+<div class="col-lg-12">
+<div class="about_part_iner">
+<div class="row align-items-center">
+<div class="col-lg-7">
+<div class="about_text">
+<h5>about us</h5>
+<h2>Delicious food provider since 1990</h2>
+<p>
+Good lights it very to above. Days image to sea. Over
+there seasons and spirit beast in. Greater bearing
+creepeth very behold fourth night morning seed moved.
+</p>
+<p>
+Good lights it very to above. Days image to sea. Over
+seasons and spirit beast in over greater bearing creepeth.
+</p>
+<a href="./about.html#" class="btn_3">learn More</a>
+</div>
+</div>
+<div class="col-lg-5">
+<div class="about_img">
+<img src="./images/xabout.png.pagespeed.ic.VlfjiV_Qeq.webp" alt="" data-pagespeed-url-hash="4273680690" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="intro_video_bg">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="intro_video_iner text-center">
+<div class="intro_video_icon">
+<a id="play-video_1" class="video-play-button popup-youtube" href="https://www.youtube.com/watch?v=pBFQdxA-apI">
+<span class="ti-control-play"></span>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="review_part section_padding">
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-xl-5">
+<div class="section_tittle">
+<p>Testimonials</p>
+<h2>What they said</h2>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-11">
+<div class="client_review_part owl-carousel owl-loaded owl-drag">
+
+
+
+
+
+
+<div class="owl-stage-outer"><div class="owl-stage" style="transition: all 0.25s ease 0s; width: 4252px; transform: translate3d(-2125px, 0px, 0px);"><div class="owl-item cloned" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_1.png.pagespeed.ic.Abc2kQ5Pxi.webp" alt="#" data-pagespeed-url-hash="729397122" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item cloned" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_2.png.pagespeed.ic.Nh5FwpSR-l.webp" alt="#" data-pagespeed-url-hash="1023897043" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item cloned" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_3.png.pagespeed.ic.i3mDTqRcqw.webp" alt="#" data-pagespeed-url-hash="1318396964" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_1.png.pagespeed.ic.Abc2kQ5Pxi.webp" alt="#" data-pagespeed-url-hash="729397122" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_2.png.pagespeed.ic.Nh5FwpSR-l.webp" alt="#" data-pagespeed-url-hash="1023897043" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_3.png.pagespeed.ic.i3mDTqRcqw.webp" alt="#" data-pagespeed-url-hash="1318396964" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item active" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_1.png.pagespeed.ic.Abc2kQ5Pxi.webp" alt="#" data-pagespeed-url-hash="729397122" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item active center" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_2.png.pagespeed.ic.Nh5FwpSR-l.webp" alt="#" data-pagespeed-url-hash="1023897043" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item active" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_3.png.pagespeed.ic.i3mDTqRcqw.webp" alt="#" data-pagespeed-url-hash="1318396964" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item cloned" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_1.png.pagespeed.ic.Abc2kQ5Pxi.webp" alt="#" data-pagespeed-url-hash="729397122" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item cloned" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_2.png.pagespeed.ic.Nh5FwpSR-l.webp" alt="#" data-pagespeed-url-hash="1023897043" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div><div class="owl-item cloned" style="width: 334.333px; margin-right: 20px;"><div class="client_review_single">
+<div class="client_review_text">
+<p>
+Good lights it very to above. Days image to sea over there
+seasons and spirit beast in greater bearing tool creepeth
+very behold.
+</p>
+<div class="client_review_img">
+<img src="./images/xclient_3.png.pagespeed.ic.i3mDTqRcqw.webp" alt="#" data-pagespeed-url-hash="1318396964" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+<h4>Mosan Cameron</h4>
+<div class="review_icon">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+</div>
+</div>
+</div>
+</div></div></div></div><div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button></div><div class="owl-dots"><button role="button" class="owl-dot"><span></span></button><button role="button" class="owl-dot"><span></span></button><button role="button" class="owl-dot"><span></span></button><button role="button" class="owl-dot"><span></span></button><button role="button" class="owl-dot active"><span></span></button><button role="button" class="owl-dot"><span></span></button></div></div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="contact_part section_padding">
+<div class="container">
+<div class="row">
+<div class="col-lg-6">
+<div class="contact_part_iner">
+<h3>Contact Us</h3>
+<div class="single_contact_part">
+<h5>address</h5>
+<p>240, Kings street, New York city USA</p>
+</div>
+<div class="single_contact_part">
+<h5>WE ARE OPEN</h5>
+<p>Mon - Fri (9.00-19.00)</p>
+<p>Sat - Sun (9.00-19.00)</p>
+</div>
+<div class="single_contact_part">
+<h5>RESERVATION</h5>
+<p>+880 367 251 167</p>
+<span>barires@contact.com</span>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+@endsection

032d20410f7fe52a89c8b381b6513b7385161401
levon379 <lev.hambardzumyan@gmail.com>
2022-12-02 15:38:10 +0400

    change menu

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -44,7 +44,7 @@
                     <button class="navbar-toggler" type="button" data-toggle="collapse"
                             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30" focusable="false"><title>Menu</title><path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M4 7h22M4 15h22M4 23h22"></path></svg>
                     </button>
                     <div class="collapse navbar-collapse main-menu-item justify-content-end"
                          id="navbarSupportedContent">

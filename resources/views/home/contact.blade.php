695b443f14295306d8d8e39e96c5c86c3f4831d1
levon379 <lev.hambardzumyan@gmail.com>
2022-12-21 16:32:50 +0400

    change form text in contacts

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -103,7 +103,7 @@
 
             <div class="row">
                 <div class="col-12">
-                    <h2 class="contact-title">Свяжитесь с нами</h2>
+                    <h2 class="contact-title">Для заявки на банкеты свяжитесь с нами</h2>
                 </div>
                 <div class="col-lg-8">
                     <form class="form-contact contact_form"
